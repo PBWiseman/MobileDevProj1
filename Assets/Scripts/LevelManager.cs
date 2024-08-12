74b6aebf5cea11c2b6d2ca659a4391e0b815afd5
PBWiseman <wisepb1@student.op.ac.nz>
2024-08-13 09:25:44 +1200

    Added XP calc and refactored searching to use LINQ

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -26,30 +26,70 @@ public class LevelManager : MonoBehaviour
         levelData = JsonUtility.FromJson<LevelData>(json);
     }
 
+    //The code for getting levels and fights was many nested foreach loops.
+    //I asked GitHub Copilot if there was a way to make it more efficient and readable and it suggested using LINQ
     public List<Entity> getEntities(int level, int fight)
     {
-        List<Entity> entities = new List<Entity>();
-        foreach (Level l in levelData.levels)
+        Level selectedLevel = GetLevel(level);
+        if (selectedLevel == null)
         {
-            if (l.level == level)
+            return new List<Entity>(); //Return empty if null
+        }
+
+        Fight selectedFight = GetFight(selectedLevel, fight);
+        if (selectedFight == null)
+        {
+            return new List<Entity>(); //Return empty if null
+        }
+
+        List<Entity> entities = new List<Entity>();
+        foreach (FightEntity fe in fightData.entities)
+        {
+            Entity entity = levelData.entities.Find(e => e.id == fe.id); //Cross reference the entity ID to the entities list.
+            if (entity != null)
             {
-                foreach (Fight f in l.fights)
-                {
-                    if (f.fight == fight)
-                    {
-                        foreach (FightEntity fe in f.entities)
-                        {
-                            Entity e = levelData.entities.Find(entity => entity.id == fe.id);
-                            for (int i = 0; i < fe.count; i++)
-                            {
-                                entities.Add(e);
-                            }
-                        }
-                        return entities;
-                    }
-                }
+                entities.AddRange(Enumerable.Repeat(entity, fe.count)); //Add the amount of enemies in the fight
             }
         }
-        return entities; //Return an empty list if not found
+
+        return entities;
+    }
+
+    //Get the xp from a fight
+    public int getFightXp(int level, int fight)
+    {
+        Level selectedLevel = GetLevel(level);
+        if (selectedLevel == null)
+        {
+            return 0; //Return 0 if null
+        }
+
+        Fight selectedFight = GetFight(selectedLevel, fight);
+        if (selectedFight == null)
+        {
+            return 0; //Return 0 if null
+        }
+
+        int xp = 0;
+        foreach (FightEntity fe in selectedFight.entities)
+        {
+            Entity entity = levelData.entities.Find(e => e.id == fe.id); //Cross reference the entity ID to the entities list.
+            if (entity != null)
+            {
+                xp += entity.experienceReward * fe.count;   //Add the xp times the amount of that enemy in the fight
+            }
+        }
+
+        return xp;
+    }
+
+    private Level GetLevel(int level)
+    {
+        return levelData.levels.FirstOrDefault(l => l.level == level);
+    }
+
+    private Fight GetFight(Level level, int fight)
+    {
+        return level.fights.FirstOrDefault(f => f.fight == fight);
     }
 }
