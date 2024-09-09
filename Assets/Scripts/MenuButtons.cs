5ed4d1cb60ca1bd729e18e3e5ed081330108f956
PBWiseman <wisepb1@student.op.ac.nz>
2024-09-10 08:37:57 +1200

    Added menu buttons

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void NewGame()
+    {
+        Debug.Log("New Game");
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game");
+    }
+}
